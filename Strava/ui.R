6e9af6e724e6554d2303c429f6048e06fc5a20a1
Samir Rehmtulla <s2rehmtu@uwaterloo.ca>
2018-10-06 15:02:42 -0400

    A bit of plot finishing and adding an image

--- Strava/ui.R
+++ Strava/ui.R
@@ -1,9 +1,15 @@
 ## ui.R ##
 library(shinydashboard)
+library(plotly)
 
 dashboardPage(
   dashboardHeader(),
-  dashboardSidebar(),
+  dashboardSidebar(
+    sidebarMenu(
+      id='tab',
+      menuItem("All-Time Stats", icon = icon("th"), tabName = "dashBoard")
+    )
+  ),
   dashboardBody(
     box(plotlyOutput("runs", height = 300), width = 12)
   )
